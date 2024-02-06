97ea1ef4f997ab28e454146b2320c2bd0f7dbde6
zachary cervenka <zachary.cervenka@gmail.com>
2024-02-05 23:12:13 -0500

    Added file input functionality

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -12,3 +12,8 @@ templates=Jinja2Templates(directory="../Frontend/templates")
 @app.get("/")
 async def index(request:Request):
     return templates.TemplateResponse("base.html",{"request":request})
+
+@app.post("/uploadFile")
+async def upload_file(file_input: UploadFile):
+    data = await file_input.read()
+    return {"fileName": file_input.filename, "fileData": data.decode("utf-8")}
